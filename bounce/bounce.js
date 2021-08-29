4c7f6a49d61b2ea3706463548a7e66e2cbee85d6
Darren Kessner <Darren.Kessner@marlborough.org>
2021-08-29 09:06:58 -0700

    added folders

---
+++ bounce/bounce.js
@@ -0,0 +1,22 @@
+let balls;
+
+function setup() {
+  createCanvas(400, 400);
+  balls = [];
+}
+
+function draw() {
+  background(0);
+
+  fill(255);
+  textAlign(CENTER);
+  textSize(20);
+  text("Click me!", width/2, height/2);
+
+  for (let b in balls)
+    balls[b].display();
+}
+
+function mousePressed() {
+  balls.push(new Ball(mouseX, mouseY));
+}
---
+++ bounce/index.md
@@ -0,0 +1,20 @@
+
+
+<script src="Ball.js"></script>
+<script src="bounce.js"></script>
+
+
+# Bounce
+
+Javascript ```class``` and array of objects.
+
+<div id="preview"></div>
+
+[bounce.js](bounce.js)
+```javascript
+{% include_relative bounce.js %} ```
+
+[Ball.js](Ball.js)
+```javascript
+{% include_relative Ball.js %} ```
+
